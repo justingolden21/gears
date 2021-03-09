66f043681234b1e01a4091796a89f25066873b3a
RGB Studios <justingolden21@gmail.com>
2021-03-08 19:18:08 -0800

    move crafting to own file
    reverted game speed as well

---
+++ docs/js/crafting.js
@@ -0,0 +1,50 @@
+const recipes = {
+	factories: {
+		gears: 100, nuts: 20, bolts: 20, time: 10,
+	},
+	crafters: {
+		gears: 10, screws: 20, nuts: 15, time: 5,
+	},
+	converters: {
+		gears: 500, bolts: 100, time: 15,
+	},
+};
+
+const factory_speeds = {
+	gears: 2,
+	nuts: 1,
+	bolts: 1,
+	screws: 1,
+};
+
+let is_crafting = false;
+
+function craft(building) {
+	if(!can_craft(building)) {
+		showSnackbar('Not enough items', 'center');
+		return;
+	}
+	if(is_crafting) {
+		showSnackbar('Currently crafting', 'center');
+		return;
+	}
+
+	for(let item in recipes[building]) {
+		if(item=='time') continue;
+		inventory.items[item] -= recipes[building][item];
+	}
+	is_crafting = true;
+	showSnackbar(`Crafting ${building}...`, 'right', recipes[building].time*1000);
+	setTimeout(()=> {
+		inventory.buildings[building] += 1;
+		is_crafting = false;
+	}, recipes[building].time*1000);
+}
+
+function can_craft(building) {
+	for(let item in recipes[building]) {
+		if(item=='time') continue;
+		if(inventory.items[item] < recipes[building][item]) return false;
+	}
+	return true;
+}
