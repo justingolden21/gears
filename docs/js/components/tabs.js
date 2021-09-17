69f050165c9f61cfc8230b544a5cc9007096ab11
RGB Studios <justingolden21@gmail.com>
2021-09-17 00:11:46 -0700

    start on about tab
    also formatted with prettier

--- docs/js/components/tabs.js
+++ docs/js/components/tabs.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
 	u('.tab-content').addClass('hidden');
-	u('.tab').on('click', e => {
+	u('.tab').on('click', (e) => {
 		let target = e.target;
 
 		// if clicked on a child of a tab like an icon, get parent
-		while(!target.classList.contains('tab')) {
+		while (!target.classList.contains('tab')) {
 			target = target.parentNode;
 		}
 
@@ -21,23 +21,19 @@ window.addEventListener('load', () => {
 	u('.tab').attr('tabindex', 0);
 
 	// if tab focused with enter then click it
-	u('.tab').on('keypress', function(e) {
-		if(e.keyCode == 13) { // enter
+	u('.tab').on('keypress', function (e) {
+		if (e.keyCode == 13) {
+			// enter
 			u(this).trigger('click');
 		}
 	});
 
 	// read url param
-	let url = new URL(window.location.href);
-	let tabName = url.searchParams.get('tab');
-	if(tabName) {
-		// open url tab
-		u(`.tab[data-tab="${tabName}"]`).trigger('click');
-	} else {
-		// open first tab
-		u('.tab:first-child').trigger('click');
-	}
-
+	const url = new URL(window.location.href);
+	const tabName = url.searchParams.get('tab') || 'about';
+	console.log(tabName);
+	// open url tab
+	u(`.tab[data-tab="${tabName}"]`).trigger('click');
 });
 
 function openTab(tabName) {
